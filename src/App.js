f30745afae84df981a4c2f56bf0a74510633abf8
unknown <sifatniloy18@gmail.com>
2022-12-21 18:39:32 -0600

    mui installed with yarn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Router } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
