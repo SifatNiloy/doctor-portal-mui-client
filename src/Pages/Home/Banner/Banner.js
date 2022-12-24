e13158befaf61a2e8bb2cf2e9a3e4a1ad0fa04ec
unknown <sifatniloy18@gmail.com>
2022-12-23 23:10:51 -0600

    banner bg and style added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,30 +1,42 @@
 import React from 'react';
-import Box from '@mui/system/Box';
 import Grid from '@mui/system/Unstable_Grid';
-import styled from '@mui/system/styled';
 import chair from '../../../images/chair.png'
 import bg from '../../../images/bg.png'
-import { Typography, Button } from '@mui/material';
+import { Typography, Button, Container } from '@mui/material';
+import { Box } from '@mui/system';
+const bannerBg = {
+    background: `url(${bg})`,
+    border:'1px solid red',
+    padding:'40px'
+}
+const verticalCenter = {
+    display: 'flex',
+    height: '450',
+    alignItems: 'center',
+
+}
 const Banner = () => {
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Container style={bannerBg } sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
-                <Grid xs={12} md={5}>
-                   <Typography variant='h3'>
-                        Your new style <br />
-                        Starts here
-                   </Typography>
-                   <Typography sx={{fontsize:14, color:'gray'}}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur inventore ea voluptate impedit necessitatibus tempore recusandae quia esse nobis cupiditate.
-                   </Typography>
-                    <Button variant='contained'>Get Appointment</Button>
+                <Grid style={{...verticalCenter ,textAlign: 'left' }} xs={12} md={6}>
+                    <Box>
+                        <Typography variant='h3'>
+                            Your new style <br />
+                            Starts here
+                        </Typography>
+                        <Typography sx={{ fontsize: 14, color: 'gray' }}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur inventore ea voluptate impedit necessitatibus tempore recusandae quia esse nobis cupiditate.
+                        </Typography>
+                        <Button variant='contained'>Get Appointment</Button>
+                    </Box>
                 </Grid>
-                <Grid xs={12} md={7}>
-                    <img style={{width:'350px'}} src={chair} alt="" />
+                <Grid xs={12} md={6} style={verticalCenter }>
+                    <img style={{ width: '350px' }} src={chair} alt="" />
                 </Grid>
-                
+
             </Grid>
-        </Box>
+        </Container>
     );
 };
 
